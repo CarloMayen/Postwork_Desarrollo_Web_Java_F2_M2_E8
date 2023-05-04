43bbbba21ffe8784da8a2aa152a9200c44850853
Carlo Mayen <carlom.116.cm@gmail.com>
2023-05-03 19:28:17 -0600

    Postwork2

---
+++ PostWork_S2/src/main/java/com/postwork_dw_java_f2_m2_e8/models/Curso.java
@@ -0,0 +1,96 @@
+package com.postwork_dw_java_f2_m2_e8.models;
+
+import java.util.Map;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "cursos")
+public class Curso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="materias_fk", referencedColumnName = "id")
+    private Materia materia;
+
+    @Column(name = "ciclo")
+    private String ciclo;
+
+    @ElementCollection
+    @CollectionTable(name = "cursos_has_estudiantes", joinColumns = {@JoinColumn(name = "cursos_fk", referencedColumnName = "id")})
+    @MapKeyJoinColumn(name = "estudiantes_fk", referencedColumnName = "id")
+    @Column(name = "calificacion")
+    private Map<Estudiante, Integer> calificaciones;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public String getCiclo() {
+        return ciclo;
+    }
+
+    public void setCiclo(String ciclo) {
+        this.ciclo = ciclo;
+    }
+
+    public Map<Estudiante, Integer> getCalificaciones() {
+        return calificaciones;
+    }
+
+    public void setCalificaciones(Map<Estudiante, Integer> calificaciones) {
+        this.calificaciones = calificaciones;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((materia == null) ? 0 : materia.hashCode());
+        result = prime * result + ((ciclo == null) ? 0 : ciclo.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Curso other = (Curso) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (materia == null) {
+            if (other.materia != null)
+                return false;
+        } else if (!materia.equals(other.materia))
+            return false;
+        if (ciclo == null) {
+            if (other.ciclo != null)
+                return false;
+        } else if (!ciclo.equals(other.ciclo))
+            return false;
+        return true;
+    }
+}
---
+++ PostWork_S2/src/main/java/com/postwork_dw_java_f2_m2_e8/multithreading/Postwork2.java
@@ -0,0 +1,53 @@
+package com.postwork_dw_java_f2_m2_e8.multithreading;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.postwork_dw_java_f2_m2_e8.models.Curso;
+import com.postwork_dw_java_f2_m2_e8.models.Materia;
+import com.postwork_dw_java_f2_m2_e8.models.Estudiante;
+
+public class Postwork2 {
+
+    private static Curso crearCurso(Random r, String nombre, Long id) {
+        Curso curso = new Curso();
+        Materia materia = new Materia();
+        Estudiante estudiante;
+
+        curso.setId(id);
+        materia.setNombre(nombre);
+        curso.setMateria(materia);
+
+        Map<Estudiante, Integer> cals = new HashMap<>();
+        for (int i = 0; i < 20; i++) {
+            estudiante = new Estudiante();
+            estudiante.setId((long) i);
+            estudiante.setNombreCompleto("E" + i);
+            cals.put(estudiante, r.nextInt(10));
+        }
+
+        curso.setCalificaciones(cals);
+        return curso;
+    }
+
+    public static void main(String[] args) {
+        Random r = new Random();
+        ExecutorService pool = Executors.newCachedThreadPool();
+        Curso[] cursos = new Curso[] {
+                crearCurso(r, "Java SE I", 1L),
+                crearCurso(r, "Java SE II", 2L),
+                crearCurso(r, "Java Backend I", 3L),
+                crearCurso(r, "Java Backend I", 4L),
+                crearCurso(r, "Mysql I", 5L)
+
+        };
+
+        for (Curso curso : cursos) {
+            pool.execute(new CalculadorPromedioCurso(curso));
+        }
+        pool.shutdown();
+    }
+}
